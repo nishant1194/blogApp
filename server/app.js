97226e91544bbf70bc864693151e827c74df90d8
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2024-07-08 10:27:13 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,6 @@ const categoryRoute = require('./admin/routes/Category') ;
 const authRoute = require('./admin/routes/Auth') ;
 const commentRoute = require('./admin/routes/Comment') ;
 
-app.use(cors());
 app.use(bodyPareser.urlencoded({extended:false})) ;
 app.use(bodyPareser.json()) ;
 
@@ -25,6 +24,8 @@ async function connectToDatabase() {
   }
   
   connectToDatabase();
+app.use(cors());
+
 
 app.use('/blog' , blogRoute) ;
 app.use('/category' , categoryRoute) ;
@@ -33,12 +34,11 @@ app.use('/comment' , commentRoute) ;
 
 
 
-app.use((req,res)=>{
-    res.json({
-        msg:"ok"
-    })
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'received request'
+  })
 })
-
 
 
 
