c212959b6f3a764fb3ec9bb22004b1e1520feb46
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2024-08-25 13:14:57 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const commentRoute = require('./admin/routes/Comment') ;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://blog-app-neew.vercel.app/',
   credentials: true
 }));
 
