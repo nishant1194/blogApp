a4d821167a994ba11732af10902c82b9b7d8edaa
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2024-08-25 16:55:01 +0530

    Update CommentList.js

--- client/src/admin/CommentList/CommentList.js
+++ client/src/admin/CommentList/CommentList.js
@@ -4,6 +4,30 @@ import axios from 'axios';
 import { Link , useNavigate} from 'react-router-dom';
 
 function CommentList() {
+   const convertToDaysAgo = (dateString) => {
+    const date = new Date(dateString);
+    const now = new Date();
+
+    // Calculate the difference in milliseconds
+    const diffInMs = now - date;
+
+    // Convert milliseconds to days
+    const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
+    const diffInHours = Math.floor((diffInMs / (1000 * 60 * 60)) % 24);
+    const diffInMins = Math.floor((diffInMs / (1000 * 60)) % 60);
+    const diffInSecs = Math.floor((diffInMs / 1000) % 60);
+    const diffInYears = Math.floor(diffInMs / (1000 * 60 * 60 * 24 * 365.25));
+
+    if (diffInMins == 0) return `${diffInSecs} secs ago`;
+
+    if (diffInHours == 0) return `${diffInMins} mins ago`;
+
+    if (diffInDays == 0) return `${diffInHours} hour ago`;
+    // Return the formatted string
+    if (diffInYears == 0) return `${diffInDays} days ago`;
+
+    return `${diffInYears} years ago`;
+  };
   const navigate = useNavigate() ;
 
   const token = localStorage.getItem("token");
@@ -54,7 +78,7 @@ function CommentList() {
      {comments.slice().reverse().map(data=>{
       return(<div className="comentBox">
         <div className="contentqq">
-        <div className="userNamew">{data.userName} <span className="timmee">2 years ago <span className='blogIdd' onClick={()=>{gotoblog(data.blogId)}}>{data.userName+data.blogId.slice(3,8)}</span> </span> </div>
+        <div className="userNamew">{data.userName} <span className="timmee">{convertToDaysAgo(data.createdAt)} <span className='blogIdd' onClick={()=>{gotoblog(data.blogId)}}>{data.userName+data.blogId.slice(3,8)}</span> </span> </div>
         <div className="comentTextw">{data.commentText}</div>
 
         </div>
