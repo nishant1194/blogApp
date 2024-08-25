1522a9a9fb2d234544dde5808dee90eafdab1514
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2024-08-25 16:53:44 +0530

    Update comment.js

--- server/models/comment.js
+++ server/models/comment.js
@@ -20,7 +20,8 @@ commentSchema = mongoose.Schema({
         type:String ,
         required: true
     }
-})
+},
+{ timestamps: true })
 
 
 module.exports = mongoose.model('Comment' , commentSchema) ;
