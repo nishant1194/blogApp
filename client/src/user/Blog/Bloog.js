94a208bb470eaa79e88389078a43cf2a42174a9c
nishant1194 <144454186+nishant1194@users.noreply.github.com>
2024-10-03 21:01:37 +0530

    Update Bloog.js

--- client/src/user/Blog/Bloog.js
+++ client/src/user/Blog/Bloog.js
@@ -71,23 +71,19 @@ function Bloog() {
   //get blog by cayegory name
 
   const getBlogsbyCategory = (category) => {
-    if (token) {
       axios
         .get(`https://blog-app-api-ten.vercel.app/blog/category/${category}`)
-
         .then((res) => {
           console.log(res.data.blog);
           setBlog(res.data.blog);
         });
    
   };
-}
 
   //get categorys
 
   const getcategorys = () => {
     console.log(token + " = token");
-    if (token) {
       axios
         .get("https://blog-app-api-ten.vercel.app/category")
         .then((res) => {
@@ -96,7 +92,7 @@ function Bloog() {
         });
     
   };
-}
+
   
   // prev page handler
   const prevPage = () => {
